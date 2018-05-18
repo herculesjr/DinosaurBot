ae1911328c1be74bd15f42b726f80cc9802bf15e
Hercules Junior <herculesjr@gmail.com>
2018-05-18 18:53:26 -0300

    Initial Commit

---
+++ DinosaurBot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DinosaurBot
+//
+//  Created by Hercules Junior on 18/05/18.
+//  Copyright © 2018 Hercules Junior. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
